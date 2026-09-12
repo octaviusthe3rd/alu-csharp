9a2cd1a2d43dbc2f375284d84e6d55f85186a367
octaviusthe3rd <a.chagaresa@alustudent.com>
2026-09-12 15:44:59 +0200

    printinf variables in slices

---
+++ csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+        static void Main(string[] args)
+        {
+		string word = "Holberton";
+		string word_first_3 = word[..3];
+		string word_last_2 = word[^2..];
+		string middle_word = word[1..^1];
+		Console.WriteLine("First 3 letters: {0}", word_first_3);
+		Console.WriteLine("Last 2 letters: {0}", word_last_2);
+		Console.WriteLine("Middle word: {0}", middle_word);
+        }
+}
