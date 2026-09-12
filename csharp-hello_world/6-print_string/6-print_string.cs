5a2e7bfb72b25e8b129a518465bc18b65741187a
octaviusthe3rd <a.chagaresa@alustudent.com>
2026-09-12 15:04:35 +0200

    writeline program printing holberton school

---
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		int count = 0;
+
+        while (count < 3){
+            Console.WriteLine(str);
+            count++;
+            }
+        Console.WriteLine(str[0..9]);
+        }
+}
