314fede913a6c84f0ae15be809d06ab8be16b7cc
octaviusthe3rd <a.chagaresa@alustudent.com>
2026-09-12 14:20:12 +0200

    writeline program

--- csharp-hello_world/3-writeline/3-writeline.cs
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Programming is like building a multilingual puzzle");
+Console.WriteLine("\"Programming is like building a multilingual puzzle");
