5dea092e2939703e7c948daaa3afa82b84686949
octaviusthe3rd <a.chagaresa@alustudent.com>
2026-09-12 15:53:15 +0200

    concating variable edges

---
+++ csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "C# (pronounced \"See Sharp\") is a simple, modern, object-oriented, and type-safe programming language. C# has its roots in the C family of languages and will be immediately familiar to C, C++, Java, and JavaScript programmers.";
+		Console.WriteLine(str.Split(' ')[8].Substring(0, 15) + ' ' + str.Split(' ')[11] + ' ' + str.Split(' ')[17] + ' ' + str.Substring(0, 2));
+        }
+}
