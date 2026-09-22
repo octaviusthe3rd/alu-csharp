906b451cbb3c8ed1dfbe27ca66a13184765258ff
octaviusthe3rd <a.chagaresa@alustudent.com>
2026-09-22 23:43:48 +0200

    printing the max value in a given list

---
+++ csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        if (myList.Count == 0)
+        {
+            Console.WriteLine("List is empty");
+            return -1;
+        }
+
+        int max = myList[0];
+
+        for (int x = 1; x < myList.Count; x++)
+        {
+            if (myList[x] > max)
+            {
+                max = myList[x];
+            }
+        }
+
+        return max;
+    }
+}
