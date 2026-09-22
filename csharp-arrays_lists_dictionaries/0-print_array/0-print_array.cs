891797506e9408443689f516425ee02a736c4c9d
octaviusthe3rd <a.chagaresa@alustudent.com>
2026-09-22 16:17:04 +0200

    printing and creating an array

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Array
+{
+    public static int?[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int?[] numbers = new int?[size];
+
+        for (int x = 0; x < size; x++)
+        {
+            numbers[x] = x;
+            Console.Write(numbers[x]);
+            if (x != size - 1)
+            {
+                Console.Write(" ");
+            }
+        }
+
+        Console.WriteLine();
+
+        return numbers;
+    }
+}
