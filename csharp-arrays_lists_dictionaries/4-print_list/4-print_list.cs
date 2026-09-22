183271ec5242e13b3e163db257ea8375b47f76f4
octaviusthe3rd <a.chagaresa@alustudent.com>
2026-09-22 23:25:54 +0200

    printing and creating a list

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int>? CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        List<int> numbers = new List<int>();
+
+        for (int x = 0; x < size; x++)
+        {
+            numbers.Add(x);
+            Console.Write(x);
+            if (x != size - 1)
+            {
+                Console.Write(" ");
+            }
+        }
+
+        Console.WriteLine();
+
+        return numbers;
+    }
+}
