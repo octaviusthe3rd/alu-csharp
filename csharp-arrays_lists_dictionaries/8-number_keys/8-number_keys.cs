cbd9deaaed1600ed92b02323590e646601416c1a
octaviusthe3rd <a.chagaresa@alustudent.com>
2026-09-23 10:14:51 +0200

    printing the number of keys in a dict

---
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int count = 0;
+
+        foreach (KeyValuePair<string, string> pair in myDict)
+        {
+            count++;
+        }
+
+        return count;
+    }
+}
