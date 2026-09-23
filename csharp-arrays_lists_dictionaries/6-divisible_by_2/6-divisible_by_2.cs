a360f7ca1dd8360cde4170c67879fb352e7b7095
octaviusthe3rd <a.chagaresa@alustudent.com>
2026-09-23 09:56:14 +0200

    printing elements of a list divisble by 2

---
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> result = new List<bool>();
+
+        for (int x = 0; x < myList.Count; x++)
+        {
+            result.Add(myList[x] % 2 == 0);
+        }
+
+        return result;
+    }
+}
