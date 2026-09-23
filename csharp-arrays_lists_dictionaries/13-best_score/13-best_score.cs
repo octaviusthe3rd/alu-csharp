9b1a826adabdb61fd9bce8766a8d335330e1df23
octaviusthe3rd <a.chagaresa@alustudent.com>
2026-09-23 10:37:04 +0200

    printing the biggest intger value in a dict

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        string bestKey = "None";
+        int bestValue = -1;
+
+        foreach (KeyValuePair<string, int> entry in myList)
+        {
+            if (entry.Value > bestValue)
+            {
+                bestValue = entry.Value;
+                bestKey = entry.Key;
+            }
+        }
+
+        return bestKey;
+    }
+}
