2d6ba03700e5042ac227dc3a0177169d4bb8a689
octaviusthe3rd <a.chagaresa@alustudent.com>
2026-09-26 17:12:27 +0200

    push pop and search

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: " + aStack.Count);
+
+        if (aStack.Count == 0)
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        else
+        {
+            Console.WriteLine("Top item: " + aStack.Peek());
+        }
+
+        bool contains = aStack.Contains(search);
+        Console.WriteLine("Stack contains \"" + search + "\": " + contains);
+
+        if (contains)
+        {
+            while (aStack.Count > 0)
+            {
+                string top = aStack.Pop();
+                if (top == search)
+                {
+                    break;
+                }
+            }
+        }
+
+        aStack.Push(newItem);
+
+        return aStack;
+    }
+}
