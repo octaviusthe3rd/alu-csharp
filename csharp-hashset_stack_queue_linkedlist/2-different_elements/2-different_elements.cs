caba4f211acd27d89cffcc2d9c72e363e9ed6216
octaviusthe3rd <a.chagaresa@alustudent.com>
2026-09-26 16:18:55 +0200

    printing a list of different elements

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> result = new List<int>();
+
+        foreach (int item in list1)
+        {
+            if (!list2.Contains(item) && !result.Contains(item))
+            {
+                result.Add(item);
+            }
+        }
+
+        foreach (int item in list2)
+        {
+            if (!list1.Contains(item) && !result.Contains(item))
+            {
+                result.Add(item);
+            }
+        }
+
+        SortList(result);
+        return result;
+    }
+
+    public static void SortList(List<int> list)
+    {
+        for (int i = 1; i < list.Count; i++)
+        {
+            int current = list[i];
+            int j = i - 1;
+
+            while (j >= 0 && list[j] > current)
+            {
+                list[j + 1] = list[j];
+                j--;
+            }
+
+            list[j + 1] = current;
+        }
+    }
+}
