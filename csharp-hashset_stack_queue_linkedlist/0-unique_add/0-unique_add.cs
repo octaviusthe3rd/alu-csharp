b688b9487e5073a7b72baeaae2f5a90654a27809
octaviusthe3rd <a.chagaresa@alustudent.com>
2026-09-26 15:16:08 +0200

    sum of unique elements in a list

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+       public static int Sum(List<int> myList)
+    {
+        int total = 0;
+        List<int> unique = new List<int>{};
+    
+        foreach (int item in myList)
+        {
+            if (!unique.Contains(item))
+            {
+                unique.Add(item);
+            }
+        }
+
+        foreach (int item in unique)
+        {
+            total += item;
+        }
+
+        return total;
+    }
+}
